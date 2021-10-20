be29ea450d939cfd5cd6892d9a497e2c73d3f70a
ZogopZ <zoiszogop@gmail.com>
2021-10-20 12:44:16 +0200

    fix figures v_0.02
    - Update heatmap to use pylib's built-in Sparql module.
    - Update code and comments to match conventions.

--- heatmap/ICOS_datacoverage_heatmap.py
+++ heatmap/ICOS_datacoverage_heatmap.py
@@ -6,22 +6,24 @@
 # Create heatmap showing data coverage of raw data available from ICOS Carbon Portal
 # as submitted by the measurements stations of the domains Ecosystem and Atmosphere
 
-
 import pandas as pd
 import datetime as dt
 import seaborn  # for data visualization
 import matplotlib.pyplot as plt
 import requests
 
-def sparql_coverage(objectspecs):
-# Query the ICOS SPARQL endpoint for the raw files of the specific object specification(s)
-    url = 'https://meta.icos-cp.eu/sparql'
+from icoscp.sparql.runsparql import RunSparql
+
+
+# Query the ICOS SPARQL endpoint for the raw files of the specific
+# object specification(s).
+def sparql_coverage(object_specs):
     query = """
             prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
             prefix prov: <http://www.w3.org/ns/prov#>
             select ?timeStart ?timeEnd ?fileName
             where {
-                VALUES ?spec {"""+objectspecs+"""}
+                VALUES ?spec {"""+object_specs+"""}
                 ?dobj cpmeta:hasObjectSpec ?spec .
                 ?dobj cpmeta:hasSizeInBytes ?size .
             ?dobj cpmeta:hasName ?fileName .
@@ -32,8 +34,8 @@ def sparql_coverage(objectspecs):
             }
             order by desc(?submTime)
     """
-    r = requests.get(url, params={'format': 'json', 'query': query})
-    data = r.json()
+    # Run the sparql query via the icos pylib Sparql module.
+    data = RunSparql(sparql_query=query, output_format='dict').run()
     cols = data['head']['vars']
     datatable = []
     for row in data['results']['bindings']:
@@ -44,57 +46,63 @@ def sparql_coverage(objectspecs):
     df_datatable = pd.DataFrame(datatable, columns=cols)
     return df_datatable
 
-def plot_coverage(domain="atm"):
-# procedure to make a heatmap plot for the raw data submitted
-# by time and station per ICOS domain
-# queries the repo with SparQL
 
-    # set the parameters that differ per domain, don't forget the "<" and ">" in objectspecs
-    if domain=="atm":
-        objectspecs="<http://meta.icos-cp.eu/resources/cpmeta/atcLosGatosL0DataObject>"+\
-                    " <http://meta.icos-cp.eu/resources/cpmeta/atcPicarroL0DataObject>"
-        stationidlen=3
-        domaintitle="Atmosphere"
+# Procedure to make a heatmap plot for the raw data submitted by time
+# and station per ICOS domain.
+def plot_coverage(domain='atm'):
+    # Set the parameters that differ per domain,
+    # don't forget the '<' and '>' in object_specs.
+    if domain == 'atm':
+        object_specs = '<http://meta.icos-cp.eu/resources/cpmeta/atcLosGatosL0DataObject>' +\
+                     '<http://meta.icos-cp.eu/resources/cpmeta/atcPicarroL0DataObject>'
+        station_id_len = 3
+        domain_title = 'Atmosphere'
     else:
-        if domain=="eco":
-            objectspecs="<http://meta.icos-cp.eu/resources/cpmeta/etcEddyFluxRawSeriesCsv>"+\
-                        " <http://meta.icos-cp.eu/resources/cpmeta/etcEddyFluxRawSeriesBin>"
-            stationidlen=6
-            domaintitle="Ecosystem"
+        if domain == 'eco':
+            object_specs = '<http://meta.icos-cp.eu/resources/cpmeta/etcEddyFluxRawSeriesCsv>' +\
+                        ' <http://meta.icos-cp.eu/resources/cpmeta/etcEddyFluxRawSeriesBin>'
+            station_id_len = 6
+            domain_title = 'Ecosystem'
         else:
-            raise RuntimeError("Unknown domain specified, only supports atm and eco")
-    fig=plt.figure(figsize=(12,6))
+            raise RuntimeError('Unknown domain specified, only supports atm and eco')
+    fig = plt.figure(figsize=(12, 6))
 
     # get the dataframe with the coverage times and filenames from the ICOS repo through
     # a SparQL query
-    atm=sparql_coverage(objectspecs)
+    atm = sparql_coverage(object_specs)
 
     # some juggling to get the ISO datetimes parsed and the dataframe indexed
     # with time ready for aggregation
-    atm["timeStart"]=pd.to_datetime(atm["timeStart"])
-    atm["timeEnd"]=pd.to_datetime(atm["timeEnd"])
-    atm["start"]=atm["timeStart"]
-    atm["period"]=atm["timeEnd"]-atm["start"]
-    atm=atm.set_index("timeStart")
-    atm["station"]=atm.fileName.str[0:stationidlen]
-    atm=atm.drop(columns="fileName")
+    atm['timeStart'] = pd.to_datetime(atm['timeStart'])
+    atm['timeEnd'] = pd.to_datetime(atm['timeEnd'])
+    atm['start'] = atm['timeStart']
+    atm['period'] = atm['timeEnd']-atm['start']
+    atm = atm.set_index('timeStart')
+    atm['station'] = atm.fileName.str[0:station_id_len]
+    atm = atm.drop(columns='fileName')
 
     # get the list of unique station names from the list and select the data per
     # station and then aggregate and concat the data to the res dataframe that
     # we transpose to get the correct plot in seaborne heatmap
-    stations=sorted(atm["station"].unique())
-    res=pd.DataFrame()
+    stations = sorted(atm['station'].unique())
+    res = pd.DataFrame()
     for station in stations:
-        trn=atm.query('station=="'+station+'"')["period"]
+        trn = atm.query('station=="' + station + '"')['period']
         trn.rename(station)
-        trn=trn.resample("W").sum()/dt.timedelta(days=7)*100
-        res=pd.concat([res,trn],axis=1,sort=False)
-    res=res.transpose()
+        trn = trn.resample('W').sum()/dt.timedelta(days=7)*100
+        res = pd.concat([res, trn], axis=1, sort=False)
+    res = res.transpose()
 
-    # call the heatmap and set the center at 95% to have everything lower change the color
-    # we have stations that run two or more instruments so we can get more than 100%
-    # coverage but that is fine, by setting vmax to 100%
-    ax=seaborn.heatmap(res,center=95,vmin=0,vmax=100,cmap="coolwarm_r",linewidths=.05,yticklabels=stations) # create seaborn heatmap
+    # Call the heatmap and set the center at 95% to have everything
+    # lower. We have stations that run two or more instruments so we
+    # can get more than 100% coverage but that is fine, by setting
+    # vmax to 100%.
+    ax = seaborn.heatmap(res,
+                         center=95,
+                         vmin=0, vmax=100,
+                         cmap='coolwarm_r',
+                         linewidths=.05,
+                         yticklabels=stations)
 
     # the heatmap does not have the x-axis as proper datetime axis so we cheat
     # to reduce the size of the labels and have them only one per month
@@ -106,12 +114,13 @@ def plot_coverage(domain="atm"):
     ax.set_xticklabels(xticklabels)
 
     # now finish the plot and save it
-    plt.title("ICOS "+domaintitle+" raw data coverage % per week and station"  )
+    plt.title('ICOS '+domain_title+' raw data coverage % per week and station'  )
     plt.savefig('heatmap'+domain+'.pdf')
     plt.savefig('heatmap'+domain+'.png')
     plt.show()
     plt.close(fig)
 
-# create the plots for the two supported domains
-plot_coverage("atm")
-plot_coverage("eco")
+
+# Create the plots for the two supported domains
+plot_coverage('atm')
+plot_coverage('eco')
