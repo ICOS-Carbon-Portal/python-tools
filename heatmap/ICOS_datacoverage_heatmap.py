a556e336d6be1040ba383ecc6ab4212e015d0042
ZogopZ <zoiszogop@gmail.com>
2021-10-26 13:46:57 +0200

    fix figures v_0.03
    - Change dataframe's sampling to monthly bins.
    - Rework dataframe's index.
    - Change labeling on the x axis.
    - Rework comments.

--- heatmap/ICOS_datacoverage_heatmap.py
+++ heatmap/ICOS_datacoverage_heatmap.py
@@ -7,12 +7,25 @@
 # as submitted by the measurements stations of the domains Ecosystem and Atmosphere
 
 import pandas as pd
+from math import ceil
 import datetime as dt
-import seaborn  # for data visualization
+import seaborn  # For data visualization.
 import matplotlib.pyplot as plt
-import requests
 
 from icoscp.sparql.runsparql import RunSparql
+
+
+# SMALL_SIZE = 8
+# MEDIUM_SIZE = 10
+# BIGGER_SIZE = 12
+#
+# plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+# plt.rc('axes', titlesize=14)     # fontsize of the axes title
+# plt.rc('axes', labelsize=12)    # fontsize of the x and y labels
+# plt.rc('xtick', labelsize=10)           # fontsize of the tick labels
+# plt.rc('ytick', labelsize=10)           # fontsize of the tick labels
+# plt.rc('legend', fontsize=16)           # legend fontsize
+# plt.rc('figure', titlesize=22)  # fontsize of the figure title
 
 
 # Query the ICOS SPARQL endpoint for the raw files of the specific
@@ -81,16 +94,29 @@ def plot_coverage(domain='atm'):
     atm['station'] = atm.fileName.str[0:station_id_len]
     atm = atm.drop(columns='fileName')
 
-    # get the list of unique station names from the list and select the data per
-    # station and then aggregate and concat the data to the res dataframe that
-    # we transpose to get the correct plot in seaborne heatmap
+    # Get the list of unique station names from the list.
     stations = sorted(atm['station'].unique())
     res = pd.DataFrame()
     for station in stations:
         trn = atm.query('station=="' + station + '"')['period']
-        trn.rename(station)
-        trn = trn.resample('W').sum()/dt.timedelta(days=7)*100
+        trn = trn.rename(station)
+        # Resample the dataframe in monthly bins.
+        trn = trn.resample('M').sum()/dt.timedelta(days=30)*100
+        # trn = trn.resample('W').sum()/dt.timedelta(days=7)*100
+        # Query out measurements collected before 2017 (due to FR-Pue
+        # problematic station which has data all the way to 2000).
+        # trn = trn.to_frame().query('timeStart > "2017-01-01 00:00:00+00:00"')
+        # Concatenate each station's data to the 'res' dataframe.
         res = pd.concat([res, trn], axis=1, sort=False)
+
+    # Create a new index for the res dataframe using parts of the
+    # previous index. This new index can be used automatically
+    # by the call to heatmap() and contains dates in the form MM-YY.
+    new_index = res.index.astype(str).to_list()
+    for i in range(len(new_index)):
+        timestamp = new_index[i]
+        new_index[i] = timestamp[5:7] + '-' + timestamp[2:4]
+    res.index = new_index
     res = res.transpose()
 
     # Call the heatmap and set the center at 95% to have everything
@@ -102,19 +128,24 @@ def plot_coverage(domain='atm'):
                          vmin=0, vmax=100,
                          cmap='coolwarm_r',
                          linewidths=.05,
-                         yticklabels=stations)
+                         yticklabels=stations,
+                         # Circa 20 x-labels for each figure.
+                         xticklabels=ceil(len(new_index)/20))
+    # Rotate the x-labels.
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=80)
 
     # the heatmap does not have the x-axis as proper datetime axis so we cheat
     # to reduce the size of the labels and have them only one per month
     # at about the right place
-    xticklabels = ax.get_xticklabels()
-    for label in xticklabels:
-        text = label.get_text()
-        label.set_text(text[5:8]+text[2:4])
-    ax.set_xticklabels(xticklabels)
+    # xticklabels = ax.get_xticklabels()
+    # for label in xticklabels:
+    #     text = label.get_text()
+    #     label.set_text(text[5:8]+text[2:4])
+    # ax.set_xticklabels(xticklabels)
 
     # now finish the plot and save it
-    plt.title('ICOS '+domain_title+' raw data coverage % per week and station'  )
+    plt.title('ICOS '+domain_title+' raw data coverage % per week and station')
+    plt.tight_layout()
     plt.savefig('heatmap'+domain+'.pdf')
     plt.savefig('heatmap'+domain+'.png')
     plt.show()
